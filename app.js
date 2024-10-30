88979eca87e47f3df6cae9f048eef08e924e527c
kultajSInghChauhan <kultajchauhan121@gmail.com>
2024-10-30 20:29:46 +0530

    boilerplate added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let mongoose = require("mongoose");
 let Listing=require("./model/listing")
 let path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require('ejs-mate')
 
 let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -22,6 +23,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/", (req, res) => {
