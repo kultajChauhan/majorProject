1ccd90516652d1e69ad973242fdc81bdf8f7d127
kultajSInghChauhan <kultajchauhan121@gmail.com>
2024-12-02 07:25:02 +0530

    authenticate setup

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const listings=require("./routes/listing.js");
 const reviews=require("./routes/review.js");
 const session=require("express-session");
 const flash=require("connect-flash");
+const passport =require("passport");
+const LocalStrategy= require("passport-local");
+const User = require("./model/user.js");
 
 
 let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -51,6 +54,13 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
